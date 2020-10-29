7b76927d59d3dbc80cd103a57526a22a74b01673
Yulia Osadchuk <yulia.osadchuk@gmail.com>
2020-10-29 15:36:36 +0200

    add Stage 1/5: Arrangement and unversioned files

--- Cinema Room Manager/task/src/cinema/Cinema.java
+++ Cinema Room Manager/task/src/cinema/Cinema.java
@@ -3,6 +3,24 @@ package cinema;
 public class Cinema {
 
     public static void main(String[] args) {
-        // Write your code here
+        String[][] array = new String[7][8];
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 0; j < array[i].length; j++) {
+                array[i][j] = "S";
+            }
+        }
+        System.out.println("Cinema:");
+        System.out.print(" ");
+        for (int i = 1; i < 9; i++) {
+            System.out.print(" " + i);
+        }
+        System.out.println();
+        for (int i = 0; i < array.length; i++) {
+            System.out.print(i + 1);
+            for (int j = 0; j < array[i].length; j++) {
+                System.out.print(" " + array[i][j]);
+            }
+            System.out.println();
+        }
     }
 }
